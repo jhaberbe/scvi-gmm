f922d4e78aef7810d1b2af21f933c1da80f00272
jhaberbe <jhaberbe@stanford.edu>
2025-04-27 03:58:56 -0700

    Small cleanup.

--- models/vae.py
+++ models/vae.py
@@ -1,10 +1,15 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.distributions import Normal, NegativeBinomial, Categorical, MixtureSameFamily, Independent
+
 class Encoder(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, latent_dim):
         super(Encoder, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, latent_dim)
-    
+
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
@@ -14,25 +19,16 @@ class MessageSender(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int = 128, latent_dim: int = 64):
         super(MessageSender, self).__init__()
 
-        self.mean_encoder = Encoder(
-            input_dim, 
-            hidden_dim=hidden_dim, 
-            latent_dim=latent_dim
-        )
-        self.logstd_encoder = Encoder(
-            input_dim, 
-            hidden_dim=hidden_dim, 
-            latent_dim=latent_dim
-        )    
+        self.mean_encoder = Encoder(input_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
+        self.logstd_encoder = Encoder(input_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
 
     def forward(self, x):
         # Is this necessary (comes from scVI basic doumentation).
         log_x = torch.log1p(X)
-
         mu = self.mean_encoder(x)
         logstd = self.logstd_encoder(x)
         return mu, logstd
-    
+
     def sample(self, x):
         mu, logstd = self.forward(x)
         return Normal(mu, torch.exp(logstd)).sample()
@@ -40,10 +36,51 @@ class MessageSender(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int = 128, latent_dim: int = 64):
         super(Decoder, self).__init__()
+
         self.fc1 = nn.Linear(latent_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, input_dim)
-    
+
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+class MessageReceiver(nn.Module):
+    def __init__(self, input_dim: int, hidden_dim: int = 128, latent_dim: int = 64):
+        super(MessageReceiver, self).__init__()
+
+        self.alpha_decoder = Decoder(X.shape[1])
+        self.beta_decoder = Decoder(X.shape[1])
+
+    def forward(self, z):
+        alpha = self.alpha_decoder(z)
+        beta = self.beta_decoder(z)
+        return F.softplus(alpha), beta
+
+class VAE(nn.Module):
+    def __init__(self, input_dim: int, hidden_dim: int = 128, latent_dim: int = 64):
+        super(VAE, self).__init__()
+        # Message Passing
+        self.message_sender = MessageSender(X.shape[1])
+        self.message_receiver = MessageReceiver(X.shape[1])
+
+        # Latent Representation Regularization
+        self.n_gaussians = 10
+        self.weights = nn.Parameter(torch.zeros(self.n_gaussians))
+        self.means = nn.Parameter(torch.zeros(self.n_gaussians, 64))
+        self.logstds = nn.Parameter(torch.zeros(self.n_gaussians, 64))
+
+        self.mixing_distribution = Categorical(probs=torch.softmax(self.weights, dim=0))
+        self.component_distribution = Independent(Normal(self.means, self.logstds.exp()), 1)
+        self.gmm = MixtureSameFamily(self.mixing_distribution, self.component_distribution)
+
+    def forward(self, x):
+        z = self.message_sender.sample(x)
+        alpha, beta = self.message_receiver(z)
+        return z, alpha, beta
+
+    def loss(self, x):
+        z, alpha, beta = self.forward(x)
+        nb_nll = -NegativeBinomial(total_count=(X.sum(axis=1, keepdims=True) * alpha), logits=beta).log_prob(X).sum(axis=1)
+        latent_nll = -self.gmm.log_prob(z)
+        return (latent_nll + nb_nll).mean()
